e698b8587d8dc6a2d054a92d85933ea95b249b00
Mario Esteban Hernandez Hernandez <hdez.marioe@gmail.com>
2020-01-27 09:34:17 -0600

    CAMBIOS EN MODELOS

--- app/ctrl_escolar/models.py
+++ app/ctrl_escolar/models.py
@@ -1,6 +1,21 @@
 from django.db import models
-from app.usuario.models import Alumno
+from app.usuario.models import Alumno, User
 # Create your models here.
+
+class Subcripcion(models.Model):
+    sub_user=models.ForeignKey(User, verbose_name='Usuario', on_delete=models.CASCADE, null=False, blank=False)
+    su_fecha_vencimiento=models.DateTimeField("Vencimiento subcripción")
+
+
+class Colegio(models.Model):
+    col_nombre=models.CharField("Nombre de colegio", max_length=200, unique=True)
+    col_fecha_add=models.DateField(auto_now_add=True)
+    col_direccion=models.CharField("Ubicacion del colegio", max_length=300)
+    col_logo=models.ImageField("Logo de institución", upload_to="img/logo/")
+
+    def __str__(self):
+        return self.col_nombre
+
 class Asistencia(models.Model):
     asis_user=models.ForeignKey(Alumno, verbose_name='Alumno', on_delete=models.CASCADE)
     asis_hra_evento=models.DateTimeField('Fecha y hora registrada', auto_now_add=True)
@@ -8,3 +23,6 @@ class Asistencia(models.Model):
     asis_tipo_evento=models.IntegerField('Tipo de evento', choices=TIPO_EVENTO)
     TIPO_TIEMPO=((1, 'A tiempo'), (2, 'Tolerancia'), (3, 'Retardo'))
     asis_tipo_tiempo=models.IntegerField('Tipo de tiempo', choices=TIPO_TIEMPO)
+
+    def __str__(self):
+        return self.col_nombre
--- app/usuario/models.py
+++ app/usuario/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from app.ctrl_escolar.models import Colegio
 # Create your models here.
 class User(AbstractUser):
     foto_perfil=models.ImageField('Foto Perfil', upload_to='foto_perfil/')
     fecha_nacimiento=models.DateField('Fecha de nacimiento',null=True, blank=True)
     telefono=models.BigIntegerField('Telefono', null=True, blank=True)
-    TIPO_USUARIO=((1, 'BASICO'), (2, 'AVANZADO'), (3, 'ADMINISTRADOR'))
-    tipo_user=models.IntegerField('Tipo de usuario', choices=TIPO_USUARIO)
-    # acerca_de_mi=models.CharField('Acerca de mi', max_length=1000, default='.....')
+    TIPO_USUARIO=((1, 'BASICO'), (2, 'AVANZADO'))
+    tipo_user=models.IntegerField('Tipo de usuario', choices=TIPO_USUARIO, default=1)
+    
     class Meta:
         db_table = 'auth_user' 
 
@@ -15,5 +16,16 @@ class User(AbstractUser):
 class Alumno(models.Model):
     al_nombres=models.CharField('Nombre alumno',max_length=100)
     al_apellidos=models.CharField('Apellidos',max_length=100)
+    al_foto=models.ImageField("Foto estuduante", upload_to="img/foto/estuduantes/",  null=True, blank=True)
     al_correo=models.EmailField('Correo')
     al_tutor=models.ForeignKey(User,verbose_name='Tutor de alumno', on_delete=models.CASCADE, null=True, blank=True)
+    al_colegio=models.ForeignKey(Colegio,verbose_name='Colegio al que pertenece', on_delete=models.CASCADE, null=True, blank=True)
+    al_huella=models.CharField("Huella digital", max_length=700,  null=True, blank=True)
+    al_entrada_init=models.TimeField("Configuracion entrada inicial",  null=True, blank=True)
+    al_entrada_end=models.TimeField("Tolerancia entrada",  null=True, blank=True)
+    
+    al_salida_init=models.TimeField("Configuracion salida inicial",  null=True, blank=True)
+    al_dalida_end=models.TimeField("Tolerancia salida",  null=True, blank=True)
+
+    def __str__(self):
+        return "{} {}".format(self.al_nombres, self.al_apellidos)
